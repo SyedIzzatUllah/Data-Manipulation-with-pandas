53a6531ad4afc551dae107d396ac537f90302f1e
Syed Izzat Ullah <53023816+SyedIzzatUllah@users.noreply.github.com>
2022-10-02 21:24:13 +0500

    Update Introducing DataFrames.py

--- Introducing DataFrames.py
+++ Introducing DataFrames.py
@@ -34,6 +34,6 @@ homelessness_ind = homelessness.sort_values('individuals')
 
 # Sort homelessness by descending family members
 homelessness_fam = homelessness.sort_values('family_members',ascending=False)
-
+ 
 # Sort homelessness by region, then descending family members
 homelessness_reg_fam = homelessness.sort_values(['region','family_members'], ascending = [True, False])
