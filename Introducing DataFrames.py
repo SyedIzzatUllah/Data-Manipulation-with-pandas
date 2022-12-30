550f319f8c2792fbee2202af652f0f8dd63326f7
Syed Izzat Ullah <53023816+SyedIzzatUllah@users.noreply.github.com>
2022-12-30 23:01:27 +0500

    Update Introducing DataFrames.py

--- Introducing DataFrames.py
+++ Introducing DataFrames.py
@@ -21,7 +21,7 @@ import pandas as pd
 # Print a 2D NumPy array of the values in homelessness.
 print(homelessness.values) 
 
-# Print the column names of homelessness
+# Print the column  names of homelessness
 print(homelessness.columns)
 
 # Print the row index of homelessness
